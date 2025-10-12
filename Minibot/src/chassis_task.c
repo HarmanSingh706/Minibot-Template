8ebeb1adc855531fcf19541708cf4767210f6f06
ekansh <eagrawa@purdue.edu>
2025-10-11 20:18:07 -0400

    added motor initializations

--- Minibot/src/chassis_task.c
+++ Minibot/src/chassis_task.c
@@ -10,7 +10,70 @@ float chassis_rad;
 
 void Chassis_Task_Init()
 {
-    // Init chassis hardware
+
+    // Create config structs for each motor
+    Motor_Config_t chassis_w1 = {
+        .can_bus = 1, // what can bus the motor is on
+        .speed_controller_id = 1, // identifier for each motor
+        .offset = 0, // Initial offset of the motor (used for encoder)
+        .control_mode = VELOCITY_CONTROL, // Control mode of the motor
+        .motor_reversal = MOTOR_REVERSAL_NORMAL, // Direction of the motor
+        .velocity_pid = // pid
+            {
+                .kp = 500.0f,
+                .kd = 0.0f,
+                .kf = 0.0f,
+                .output_limit = M2006_MAX_CURRENT, // m2006 is the motor
+            },
+    };
+    Motor_Config_t chassis_w2 = {
+        .can_bus = 1,
+        .speed_controller_id = 2,
+        .offset = 0,
+        .control_mode = VELOCITY_CONTROL,
+        .motor_reversal = MOTOR_REVERSAL_REVERSED,
+        .velocity_pid =
+            {
+                .kp = 500.0f,
+                .kd = 0.0f,
+                .kf = 0.0f,
+                .output_limit = M2006_MAX_CURRENT,
+            },
+    };
+    Motor_Config_t chassis_w3 = {
+        .can_bus = 1,
+        .speed_controller_id = 3,
+        .offset = 0,
+        .control_mode = VELOCITY_CONTROL,
+        .motor_reversal = MOTOR_REVERSAL_NORMAL,
+        .velocity_pid =
+            {
+                .kp = 500.0f,
+                .kd = 0.0f,
+                .kf = 0.0f,
+                .output_limit = M2006_MAX_CURRENT,
+            },
+    };
+    Motor_Config_t chassis_w4 = {
+        .can_bus = 1,
+        .speed_controller_id = 4,
+        .offset = 0,
+        .control_mode = VELOCITY_CONTROL,
+        .motor_reversal = MOTOR_REVERSAL_REVERSED,
+        .velocity_pid =
+            {
+                .kp = 500.0f,
+                .kd = 0.0f,
+                .kf = 0.0f,
+                .output_limit = M2006_MAX_CURRENT,
+            },
+    };
+
+    //create motor instances
+    motor_w1 = DJI_Motor_Init(&chassis_w1, M2006); 
+    motor_w2 = DJI_Motor_Init(&chassis_w2, M2006);
+    motor_w3 = DJI_Motor_Init(&chassis_w3, M2006);
+    motor_w4 = DJI_Motor_Init(&chassis_w4, M2006);
 }
 
 void Chassis_Ctrl_Loop()
